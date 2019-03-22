91322db7f4ddf48298efd6e552d380ebf9231b8b
Mohammad Sadan Khan <39806875+sadankhan@users.noreply.github.com>
2019-03-22 14:16:15 +0530

    Update Socket Module Description.py

--- Socket Module Description.py
+++ Socket Module Description.py
@@ -11,52 +11,33 @@ socket_type − This is either SOCK_STREAM or SOCK_DGRAM.
 protocol − This is usually left out, defaulting to 0.
 
 Once you have socket object, then you can use required functions to create your client or server program. Following is the list of functions required −
-
+#############################################
 Server Socket Methods
 Method & Description
 
-s.bind()
+s.bind() #This method binds address (hostname, port number pair) to socket.
 
-This method binds address (hostname, port number pair) to socket.
+s.listen() #This method sets up and start TCP listener.
 
-s.listen()
-
-This method sets up and start TCP listener.
-
-s.accept()
-
-This passively accept TCP client connection, waiting until connection arrives (blocking).
-
+s.accept() #This passively accept TCP client connection, waiting until connection arrives (blocking).
+###############################################
 Client Socket Methods
 Sr.No.	Method & Description
 
-s.connect()
-
-This method actively initiates TCP server connection.
-
+s.connect() #This method actively initiates TCP server connection.
+###############################################
 General Socket Methods
 Sr.No.	Method & Description
 
-s.recv()
+s.recv() #This method receives TCP message
 
-This method receives TCP message
+s.send() #This method transmits TCP message
 
-s.send()
+s.recvfrom() #This method receives UDP message
 
-This method transmits TCP message
+s.sendto() #This method transmits UDP message
 
-s.recvfrom()
+s.close() #This method closes socket
 
-This method receives UDP message
-
-s.sendto()
-
-This method transmits UDP message
-
-s.close()
-
-This method closes socket
-
-socket.gethostname()
-
-Returns the hostname.
+socket.gethostname() #Returns the hostname.
+##############################################
